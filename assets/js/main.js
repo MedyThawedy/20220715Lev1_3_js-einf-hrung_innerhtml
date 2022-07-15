306287c776d62e921dcd8babb46e13cd89e13e6e
MD <dhmedy@googlemail.com>
2022-07-15 12:08:27 +0200

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,6 @@ vFigure2 += ' alt="Trulli" style="width:100%"><figcaption>Fig.2 - Marsaxlokk</fi
 let vFigure3 = '<figure><img src="https://images.unsplash.com/photo-1590261177475-f1f245ec70d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80" ';
 vFigure3 += 'alt="Trulli" style="width:100%"><figcaption>Fig.3 - Blue Grotto</figcaption></figure>';
 
-vSection.innerHTML = vSection + vFigure1;
+vSection.innerHTML = vSection.innerHTML + vFigure1;
 vSection.innerHTML += vFigure2;
 vSection.innerHTML += vFigure3; 
